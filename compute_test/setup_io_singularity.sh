54902ed1191b54e7ade32c52dcbaeff8c257b1d8
Przemysław Morski <przemyslaw.morski@euvic.pl>
2020-04-08 20:20:34 +0200

    fix to singularity scripts --writable flag

--- compute_test/setup_io_singularity.sh
+++ compute_test/setup_io_singularity.sh
@@ -1,2 +1,2 @@
 singularity build --sandbox --fix-perms compute_test_sandbox/ docker://pmorski/compute_test_fibonacci
-singularity run --writeable compute_test_sandbox &
+singularity run --writable compute_test_sandbox &
