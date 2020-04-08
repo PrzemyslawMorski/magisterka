c55d7c13004e86a84ff2134038a95f9625df5d66
Przemysław Morski <przemyslaw.morski@euvic.pl>
2020-04-08 17:45:03 +0200

    update to setup elastic scripts

--- io_test/setup_io_singularity.sh
+++ io_test/setup_io_singularity.sh
@@ -2,7 +2,6 @@ singularity build io_test.sif docker://docker.elastic.co/elasticsearch/elasticse
 
 singularity run --network-args "portmap=9202:9200/tcp" io_test.sif &
 
-sleep 10s
+sleep 30s
 
-curl -XPUT 'localhost:9202/companydatabase?pretty' -H 'Content-Type: application/json' -d' {"mappings" : { "employees" : { "properties" : { "FirstName" : { "type" : "text" }, "LastName" : { "type" : "text" }, "Designation" : { "type" : "text" }, "Salary" : { "type" : "integer" }, "DateOfJoining" : { "type" : "date", "format": "yyyy-MM-dd" }, "Address" : { "type" : "text" }, "Gender" : { "type" : "text" }, "Age" : { "type" : "integer" }, "MaritalStatus" : { "type" : "text" }, "Interests" : { "type" : "text" }}}}}' 
-curl -XPUT 'localhost:9202/companydatabase/_bulk' --data-binary @Employees50K.json
+curl -XPUT 'localhost:9202/companydatabase/_bulk' -H 'Content-Type: application/json' --data-binary @Employees100K.json
