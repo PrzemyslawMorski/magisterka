38f8be2a33c490e45673603673255174a45668e8
Przemysław Morski <przemyslaw.morski@euvic.pl>
2020-04-08 20:25:48 +0200

    setup scripts renamed

--- run_all_containers.sh
+++ run_all_containers.sh
@@ -1,15 +1,15 @@
 cd compute_test
 bash build_push_docker_hub.sh
-bash setup_io_docker.sh
-bash setup_io_singularity.sh
+bash setup_docker.sh
+bash setup_singularity.sh
 cd ..
 
 cd io_test
-bash setup_io_docker.sh
-bash setup_io_singularity.sh
+bash setup_docker.sh
+bash setup_singularity.sh
 cd ..
 
 cd network_test
-bash setup_io_docker.sh
-bash setup_io_singularity.sh
+bash setup_docker.sh
+bash setup_singularity.sh
 cd ..
