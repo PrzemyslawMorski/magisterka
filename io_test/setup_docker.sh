a94c7239edf11f1ff2497d15ff06cb4eb8538f04
Przemysław Morski <przemyslaw.morski@euvic.pl>
2020-04-08 21:09:47 +0200

    fix to docker io test volume and network test port

--- io_test/setup_docker.sh
+++ io_test/setup_docker.sh
@@ -1,4 +1,4 @@
-docker run -d -e discovery.type=single-node -v ./elasticsearch.yml:/usr/share/elasticsearch/config/elasicsearch.yml -p 9201:9200 docker.elastic.co/elasticsearch/elasticsearch:7.4.2
+docker run -d -e discovery.type=single-node -v $(pwd)/elasticsearch.yml:/usr/share/elasticsearch/config/elasicsearch.yml -p 9201:9200 docker.elastic.co/elasticsearch/elasticsearch:7.4.2
 
 sleep 30s
 
--- network_test/setup_docker.sh
+++ network_test/setup_docker.sh
@@ -1,1 +1,1 @@
-docker run -p 8080:80 --name network_test --rm -d nginx
+docker run -p 9080:80 --name network_test --rm -d nginx
