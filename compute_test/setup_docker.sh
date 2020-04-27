52f5a57b10638d90565b44b89cc1feaf79d9cc04
Przemysław Morski <przemyslaw.morski@euvic.pl>
2020-04-27 19:05:48 +0200

    compute jmeter docker test

--- compute_test/setup_docker.sh
+++ compute_test/setup_docker.sh
@@ -1,1 +1,2 @@
-docker run -p 8081:8080 --name compute_test -d pmorski/compute_test_fibonacci
+glances --export csv --export-csv-file usage_docker.csv -q &
+docker run -p 8081:8080 --rm --name compute_test pmorski/compute_test_fibonacci
