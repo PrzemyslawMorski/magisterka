ee282d6259e1a2a528212e16ad110ed8f3f093f3
Przemysław Morski <przemyslaw.morski@euvic.pl>
2020-04-27 19:51:51 +0200

    renamed jmeter test configs

--- network_run_podman_and_log_usage.sh
+++
@@ -1,8 +0,0 @@
-glances_id=$(glances --export csv --export-csv-file usage_podman.csv -q &)
-podman_id=$(podman run -p 9081:80 --name network_test --rm -d nginx)
-
-read -p "Kill the container? " answer
-
-podman kill $podman_id
-podman container rm $podman_id
-kill $glances_id
