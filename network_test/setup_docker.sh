73f83ba696ff76ac66360723edf9c5ff9dd2f464
Przemysław Morski <przemyslaw.morski@euvic.pl>
2020-04-27 19:06:01 +0200

    jmeter docker network test

--- network_test/setup_docker.sh
+++ network_test/setup_docker.sh
@@ -1,8 +1,2 @@
-glances_id=$(glances --export csv --export-csv-file usage_docker.csv -q &)
-docker_id=$(docker run -p 9080:80 --name network_test --rm -d nginx)
-
-read -p "Kill the container? " answer
-
-docker kill $docker_id
-docker container rm $docker_id
-kill $glances_id
+glances --export csv --export-csv-file usage_docker.csv -q &
+docker run -p 9080:80 --name network_test --rm nginx
