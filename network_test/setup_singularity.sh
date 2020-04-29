d998a6619a4e668a18af5abe2d5fec52537a69a0
Przemysław Morski <przemyslaw.morski@euvic.pl>
2020-04-29 14:55:23 +0200

    sudo run added to singularity network setup

--- network_test/setup_singularity.sh
+++ network_test/setup_singularity.sh
@@ -2,4 +2,4 @@ singularity build --sandbox --fix-perms network_test_sandbox/ docker://nginx
 
 glances --export csv --export-csv-file usage_singularity.csv -q &
 
-singularity run --writable network_test_sandbox &
+sudo singularity run --writable network_test_sandbox &
