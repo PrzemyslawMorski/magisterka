509fae210d44b90150b63078ebc1e3d29f333ffd
Przemysław Morski <przemyslaw.morski@euvic.pl>
2020-04-29 13:54:14 +0200

    updated ports for docker config

--- compute_test/setup_docker.sh
+++ compute_test/setup_docker.sh
@@ -1,2 +1,2 @@
 glances --export csv --export-csv-file usage_docker.csv -q &
-docker run -p 8081:8080 --rm --name compute_test pmorski/compute_test_fibonacci
+docker run -p 8080:8080 --rm --name compute_test pmorski/compute_test_fibonacci
--- io_test/setup_docker.sh
+++ io_test/setup_docker.sh
@@ -1,7 +1,7 @@
 glances --export csv --export-csv-file usage_docker.csv -q &
 
-docker run -d --rm -e discovery.type=single-node -e "ES_JAVA_OPTS=-Xms6g -Xmx6g" -v $(pwd)/elasticsearch.yml:/usr/share/elasticsearch/config/elasicsearch.yml -p 9201:9200 docker.elastic.co/elasticsearch/elasticsearch:7.4.2
+docker run -d --rm -e discovery.type=single-node -e "ES_JAVA_OPTS=-Xms6g -Xmx6g" -v $(pwd)/elasticsearch.yml:/usr/share/elasticsearch/config/elasicsearch.yml -p 9200:9200 docker.elastic.co/elasticsearch/elasticsearch:7.4.2
 
 sleep 30s
 
-curl -XPUT 'localhost:9201/companydatabase/_bulk' -H 'Content-Type: application/json' --data-binary @Employees100K.json
+curl -XPUT 'localhost:9200/companydatabase/_bulk' -H 'Content-Type: application/json' --data-binary @Employees100K.json
