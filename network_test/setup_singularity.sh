e1c3f241be3b5f18f965a85de2d6a9bced8ec5c7
Przemysław Morski <przemyslaw.morski@euvic.pl>
2020-04-29 14:10:39 +0200

    update to singularity scripts

--- network_test/setup_singularity.sh
+++ network_test/setup_singularity.sh
@@ -2,4 +2,4 @@ singularity build --sandbox --fix-perms network_test_sandbox/ docker://nginx
 
 glances --export csv --export-csv-file usage_singularity.csv -q &
 
-sudo singularity run --writable network_test_sandbox &
+singularity run --writable network_test_sandbox &
