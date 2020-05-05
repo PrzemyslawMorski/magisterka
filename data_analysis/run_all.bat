11d207f70e5ca48994d19ca823ccf6b76561bbd8
Przemysław Morski <przemyslaw.morski@euvic.pl>
2020-05-05 20:46:35 +0200

    poprawka do run_all.bat

--- data_analysis/run_all.bat
+++ data_analysis/run_all.bat
@@ -1,7 +1,8 @@
-set file_name="C:\Users\pmorski\Desktop\Wyniki testow\podman fibb 30\summary.csv"
+set root_dir="."
+set file_name=%root_dir%"\summary.csv"
 
-Rscript .\divide_into_chunks.R -s "elapsed" -o ".\elapsed_output.csv" -f %file_name%
-Rscript .\divide_into_chunks.R -s "Latency" -o ".\latency_output.csv" -f %file_name%
-Rscript .\divide_into_chunks.R -s "ErrorCount" -o ".\error_count_output.csv" -f %file_name%
-Rscript .\divide_into_chunks.R -s "Connect" -o ".\connect_output.csv" -f %file_name%
-Rscript .\divide_into_chunks.R -s "IdleTime" -o ".\idle_time_output.csv" -f %file_name%
+Rscript .\divide_into_chunks.R -s "elapsed" -o %root_dir%"\elapsed_output.csv" -f %file_name%
+Rscript .\divide_into_chunks.R -s "Latency" -o %root_dir%"\latency_output.csv" -f %file_name%
+Rscript .\divide_into_chunks.R -s "ErrorCount" -o %root_dir%"\error_count_output.csv" -f %file_name%
+Rscript .\divide_into_chunks.R -s "Connect" -o %root_dir%"\connect_output.csv" -f %file_name%
+Rscript .\divide_into_chunks.R -s "IdleTime" -o %root_dir%"\idle_time_output.csv" -f %file_name%
