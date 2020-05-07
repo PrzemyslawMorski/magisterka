cfcdc7aa964c3e812675414a9ef60715aceb47c6
Przemysław Morski <przemyslaw.morski@euvic.pl>
2020-05-07 23:03:24 +0200

    poprawka do run_jmeter_test - glances zabijane prawidlowo

--- run_jmeter_test.py
+++ run_jmeter_test.py
@@ -3,17 +3,10 @@ import time
 import paramiko
 import shutil
 import os
+import sys
 
 jmeter_path = "C:\Program Files\\apache-jmeter-5.2.1\\bin\jmeter.bat"
-test_file = r"C:\GIT\magisterka\network_test\Network test.jmx"
-results_path = "C:\\GIT\\magisterka\\network_test\\podman\\"
-num_tests = 10
-
-setup_script = ''
-teardown_script = ''
-run_script = r'C:\GIT\magisterka\network_test\run_podman.sh'
-stop_script = r'C:\GIT\magisterka\network_test\stop_podman.sh'
-
+num_tests = 2
 test_machine_addr = "192.168.0.168"
 usr = "pmorski"
 password = "pmorski"
@@ -47,48 +40,139 @@ def ssh_exec(command):
     ssh.close()
 
 
-if setup_script != '':
-    sftp_put(setup_script, 'setup.sh')
-    ssh_exec('bash setup.sh')
+def run_test(test_file, results_path, setup_script, teardown_script, run_script, stop_script):
 
-if teardown_script != '':
-    sftp_put(teardown_script, 'teardown.sh')
+    if setup_script != '':
+        sftp_put(setup_script, 'setup.sh')
+        ssh_exec('bash setup.sh')
+        time.sleep(5)
 
-sftp_put(run_script, 'run.sh')
-sftp_put(stop_script, 'stop.sh')
+    if teardown_script != '':
+        sftp_put(teardown_script, 'teardown.sh')
+
+    sftp_put(run_script, 'run.sh')
+    sftp_put(stop_script, 'stop.sh')
+
+    for i in range(num_tests):
+        results_file = "results_"+str(i+1)+".csv"
+        usage_file = "usage_"+str(i+1)+".csv"
+
+        print("Starting iteration: "+str(i+1))
+
+        ssh_exec(
+            "glances --export csv --export-csv-file " + usage_file + " --time 0.4 -q &")
+
+        print("Started glances")
+
+        ssh_exec('bash run.sh')
+        time.sleep(5)
+
+        print("Started container")
+
+        subprocess.run([jmeter_path,  "-n", "-t", test_file,
+                        "-l", results_path+results_file])
+        print("Finished jmeter")
+
+        ssh_exec('bash stop.sh')
+        print("Stopped container")
+
+        ssh_exec('kill $(pgrep -f glances)')
+        print("Killed glances")
+
+        time.sleep(10)
+
+        # shutil.move(".\\summary.csv", results_path+results_file)
+        sftp_get(usage_file, results_path+usage_file)
+        ssh_exec('rm ' + usage_file)
+
+    if teardown_script != '':
+        ssh_exec('bash teardown.sh')
 
 
-for i in range(num_tests):
-    results_file = "results_"+str(i+1)+".csv"
-    usage_file = "usage_"+str(i+1)+".csv"
+# network
+# run_test(test_file=r"C:\GIT\magisterka\network_test\Network test.jmx",
+#          results_path="C:\\GIT\\magisterka\\network_test\\docker\\",
+#          setup_script=r'C:\GIT\magisterka\network_test\setup_docker.sh',
+#          teardown_script=r'C:\GIT\magisterka\network_test\teardown_docker.sh',
+#          run_script=r'C:\GIT\magisterka\network_test\run_docker.sh',
+#          stop_script=r'C:\GIT\magisterka\network_test\stop_docker.sh')
 
-    print("Starting iteration: "+str(i+1))
+# run_test(test_file=r"C:\GIT\magisterka\network_test\Network test.jmx",
+#          results_path="C:\\GIT\\magisterka\\network_test\\podman\\",
+#          setup_script=r'C:\GIT\magisterka\network_test\setup_podman.sh',
+#          teardown_script=r'C:\GIT\magisterka\network_test\teardown_podman.sh',
+#          run_script=r'C:\GIT\magisterka\network_test\run_podman.sh',
+#          stop_script=r'C:\GIT\magisterka\network_test\stop_podman.sh')
 
-    ssh_exec(
-        "glances --export csv --export-csv-file " + usage_file + " --time 0.4 -q &")
+run_test(test_file=r"C:\GIT\magisterka\network_test\Network test.jmx",
+         results_path="C:\\GIT\\magisterka\\network_test\\singularity\\",
+         setup_script=r'C:\GIT\magisterka\network_test\setup_singularity.sh',
+         teardown_script=r'C:\GIT\magisterka\network_test\teardown_singularity.sh',
+         run_script=r'C:\GIT\magisterka\network_test\run_singularity.sh',
+         stop_script=r'C:\GIT\magisterka\network_test\stop_singularity.sh')
 
-    print("Started glances")
+# compute
+# run_test(test_file=r"C:\GIT\magisterka\compute_test\Compute test.jmx",
+#          results_path="C:\\GIT\\magisterka\\compute_test\\podman\\",
+#          setup_script=r'C:\GIT\magisterka\compute_test\setup_podman.sh',
+#          teardown_script=r'C:\GIT\magisterka\compute_test\teardown_podman.sh',
+#          run_script=r'C:\GIT\magisterka\compute_test\run_podman.sh',
+#          stop_script=r'C:\GIT\magisterka\compute_test\stop_podman.sh')
 
-    ssh_exec('bash run.sh')
+# run_test(test_file=r"C:\GIT\magisterka\compute_test\Compute test.jmx",
+#          results_path="C:\\GIT\\magisterka\\compute_test\\docker\\",
+#          setup_script=r'C:\GIT\magisterka\compute_test\setup_docker.sh',
+#          teardown_script=r'C:\GIT\magisterka\compute_test\teardown_docker.sh',
+#          run_script=r'C:\GIT\magisterka\compute_test\run_docker.sh',
+#          stop_script=r'C:\GIT\magisterka\compute_test\stop_docker.sh')
 
-    print("Started container")
+# run_test(test_file=r"C:\GIT\magisterka\compute_test\Compute test.jmx",
+#          results_path="C:\\GIT\\magisterka\\compute_test\\singularity\\",
+#          setup_script=r'C:\GIT\magisterka\compute_test\setup_singularity.sh',
+#          teardown_script=r'C:\GIT\magisterka\compute_test\teardown_singularity.sh',
+#          run_script=r'C:\GIT\magisterka\compute_test\run_singularity.sh',
+#          stop_script=r'C:\GIT\magisterka\compute_test\stop_singularity.sh')
 
-    subprocess.run([jmeter_path,  "-n", "-t", test_file, "-l", results_path+results_file])
-    print("Finished jmeter")
+# io query
+# run_test(test_file=r"C:\GIT\magisterka\io_test\Search test.jmx",
+#          results_path="C:\\GIT\\magisterka\\io_test\\query\\podman\\",
+#          setup_script=r'C:\GIT\magisterka\io_test\setup_podman.sh',
+#          teardown_script=r'C:\GIT\magisterka\io_test\teardown_podman.sh',
+#          run_script=r'C:\GIT\magisterka\io_test\run_podman.sh',
+#          stop_script=r'C:\GIT\magisterka\io_test\stop_podman.sh')
 
+# run_test(test_file=r"C:\GIT\magisterka\io_test\Search test.jmx",
+#          results_path="C:\\GIT\\magisterka\\io_test\\query\\docker\\",
+#          setup_script=r'C:\GIT\magisterka\io_test\setup_docker.sh',
+#          teardown_script=r'C:\GIT\magisterka\io_test\teardown_docker.sh',
+#          run_script=r'C:\GIT\magisterka\io_test\run_docker.sh',
+#          stop_script=r'C:\GIT\magisterka\io_test\stop_docker.sh')
 
-    ssh_exec('bash stop.sh')
-    print("Stopped container")
+# run_test(test_file=r"C:\GIT\magisterka\io_test\Search test.jmx",
+#          results_path="C:\\GIT\\magisterka\\io_test\\query\\singularity\\",
+#          setup_script=r'C:\GIT\magisterka\io_test\setup_singularity.sh',
+#          teardown_script=r'C:\GIT\magisterka\io_test\teardown_singularity.sh',
+#          run_script=r'C:\GIT\magisterka\io_test\run_singularity.sh',
+#          stop_script=r'C:\GIT\magisterka\io_test\stop_singularity.sh')
 
+# io index
+# run_test(test_file=r"C:\GIT\magisterka\io_test\Index test.jmx",
+#          results_path="C:\\GIT\\magisterka\\io_test\\index\\podman\\",
+#          setup_script=r'C:\GIT\magisterka\io_test\setup_podman.sh',
+#          teardown_script=r'C:\GIT\magisterka\io_test\teardown_podman.sh',
+#          run_script=r'C:\GIT\magisterka\io_test\run_podman.sh',
+#          stop_script=r'C:\GIT\magisterka\io_test\stop_podman.sh')
 
-    ssh_exec('pkill glances')
-    print("Killed glances")
+# run_test(test_file=r"C:\GIT\magisterka\io_test\Index test.jmx",
+#          results_path="C:\\GIT\\magisterka\\io_test\\index\\docker\\",
+#          setup_script=r'C:\GIT\magisterka\io_test\setup_docker.sh',
+#          teardown_script=r'C:\GIT\magisterka\io_test\teardown_docker.sh',
+#          run_script=r'C:\GIT\magisterka\io_test\run_docker.sh',
+#          stop_script=r'C:\GIT\magisterka\io_test\stop_docker.sh')
 
-    # time.sleep("60")
-
-    # shutil.move(".\\summary.csv", results_path+results_file)
-    sftp_get(usage_file, results_path+usage_file)
-    ssh_exec('rm ' + usage_file)
-
-if teardown_script != '':
-    ssh_exec('bash teardown.sh')
+# run_test(test_file=r"C:\GIT\magisterka\io_test\Index test.jmx",
+#          results_path="C:\\GIT\\magisterka\\io_test\\index\\singularity\\",
+#          setup_script=r'C:\GIT\magisterka\io_test\setup_singularity.sh',
+#          teardown_script=r'C:\GIT\magisterka\io_test\teardown_singularity.sh',
+#          run_script=r'C:\GIT\magisterka\io_test\run_singularity.sh',
+#          stop_script=r'C:\GIT\magisterka\io_test\stop_singularity.sh')
